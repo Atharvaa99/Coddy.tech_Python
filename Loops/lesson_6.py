21e60db2cb24af0ce6a955e10d3a666763333d27
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-06 20:36:52 +0530

    Commit

---
+++ Loops/lesson_6.py
@@ -0,0 +1,10 @@
+# Get input for rows and columns
+rows = int(input())
+cols = int(input())
+
+for i in range(rows):
+    line = ""
+    for j in range(cols):
+        line += "*"
+    print(line)
+g
