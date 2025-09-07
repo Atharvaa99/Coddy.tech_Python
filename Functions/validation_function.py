9c6fee5abf33b8c4c685afa9a3bacb40e47eeae9
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-07 20:36:01 +0530

    Commit

---
+++ Functions/validation_function.py
@@ -0,0 +1,7 @@
+def is_valid(username, password):
+    if username == "user" and password == "qweasd":
+        return True
+    elif username == "admin" :
+        return True
+    else:
+        return False
