d73efd922375064f4d268890b8f2afff2ce8ad77
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-13 16:59:41 +0530

    Commit

---
+++ Daily_expense_tracker/stage4.py
@@ -0,0 +1,28 @@
+print("Welcome to the Daily Expense Tracker!\n")
+print("Menu:")
+print("1. Add a new expense")
+print("2. View all expenses")
+print("3. Calculate total and average expense")
+print("4. Clear all expenses")
+print("5. Exit")
+lst = []
+while True:
+    choice = int(input())
+    if choice == 1:
+        expense = float(input())
+        lst.append(expense)
+        print("Expense added successfully!")
+    elif choice == 2:
+        if len(lst) > 0:
+            print("Your Expenses:")
+            for index,expense in enumerate(lst):
+                print(f"{index+1}. {expense}")
+        else:
+            print("No expenses recorded yet.")
+    elif choice == 3:
+        continue
+    elif choice == 4:
+        continue
+    elif choice == 5:
+        print("Exiting the Daily Expense Tracker. Goodbye!")
+        break
