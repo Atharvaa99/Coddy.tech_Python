1a0664400d8d6dcd8f75039aac7022e2d6438755
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-16 14:23:15 +0530

    Commit

---
+++ Dictionary_Part-2/lesson_2.py
@@ -0,0 +1,5 @@
+employees = {"Alice": "HR", "Bob": "Engineering", "Diana": "Marketing"}
+if 'Alice' in employees.keys():
+    print("Alice is in the company.")
+if 'Jhon' not in employees:
+    print("John is not in the company.")
