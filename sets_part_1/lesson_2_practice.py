9d921569cf5344fd4684d4c67a350bb0866b1de2
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-19 23:46:56 +0530

    Commit

---
+++ sets_part_1/lesson_2_practice.py
@@ -0,0 +1,9 @@
+def manage_list(list1,element_to_append,index_to_remove):
+    list1.append(element_to_append)
+    if index_to_remove < len(list1):
+        list1.pop(index_to_remove)
+    
+    if len(list1) > 3:
+        return "The list has more than 3 elements"
+    else:
+        return "The list has 3 or fewer elements"
