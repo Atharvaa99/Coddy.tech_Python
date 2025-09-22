6b1769aa1f96f09ec42e469b842c28203c831517
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-22 19:38:51 +0530

    Commit

---
+++ Advanced_data_aggregation/lesson_2.py
@@ -0,0 +1,6 @@
+numbers = [42,17,23,56,9,34]
+words = ['kiwi','apple','banana','cherry','date']
+print(f"Smallest number: {min(numbers)}")
+print(f"Largest number: {max(numbers)}")
+print(f"Smallest word: {min(words)}")
+print(f"Largest word: {max(words)}")
