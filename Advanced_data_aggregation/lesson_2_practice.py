4f29fa5ef2862d9675f2565d225166679365da54
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-22 19:40:14 +0530

    Commit

---
+++ Advanced_data_aggregation/lesson_2_practice.py
@@ -0,0 +1,11 @@
+temperature = [72,68,75,80,65,70,78]
+humidity = [60,55,65,70,50,58,62]
+highest_temp = max(temperature)
+lowest_temp = min(temperature)
+highest_humidity = max(humidity)
+lowest_humidity = min(humidity)
+
+print(f"Highest temperature: {highest_temp}°F")
+print(f"Lowest temperature: {lowest_temp}°F")
+print(f"Highest humidity: {highest_humidity}%")
+print(f"Lowest humidity: {lowest_humidity}%")
