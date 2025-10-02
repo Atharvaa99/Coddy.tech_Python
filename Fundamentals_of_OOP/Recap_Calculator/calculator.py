04a99a4b02734f4224269bd9e6b7dbaeea47ed29
Atharvaa99 <atharvadhaigude@icloud.com>
2025-10-02 19:08:31 +0530

    Commit

---
+++ Fundamentals_of_OOP/Recap_Calculator/calculator.py
@@ -0,0 +1,30 @@
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self,number):
+        self.result += number
+        return self.result
+
+    def subtract(self,number):
+        self.result -= number
+        return self.result
+
+    def multiply(self,number):
+        self.result *= number
+        return self.result
+
+    def divide(self,number):
+        if number == 0:
+            print("Error: Division by zero")
+
+        else:
+            self.result /= number
+        return self.result
+
+    def clear(self):
+        self.result = 0
+        return self.result
+
+    def get_result(self):
+        return self.result
